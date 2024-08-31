e346be04e475908f6ad2fbe7fa10cd638172dfb9
Roshan <roshan9825930688@gmail.com>
2024-08-31 23:23:41 +0545

    New file, template added

---
+++ Chapter10/template.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+T find_max(T a, T b){
+    T result;
+    if(a > b)
+        result = a;
+    else
+        result = b;
+    return result;
+}
+
+int main()
+{
+    int i1 = 15, i2 = 20;
+    cout<< "Greater is "<< find_max(i1, i2)<< endl;
+    float f1 = 40000.05, f2 = 38000.44;
+    cout<< "Greater is "<< find_max(f1, f2)<< endl;
+    double d1 = 55.05, d2 = 67.777;
+    cout<< "Greater is "<< find_max(d1, d2)<< endl;
+    char c1 = 'a', c2 = 'A';
+    cout<< "Greater is "<< find_max(c1, c2)<< endl;
+    return 0;
+}
