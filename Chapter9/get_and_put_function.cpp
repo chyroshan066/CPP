bf0074d904b06a43214fe71645381eccd7541aa2
Roshan <roshan9825930688@gmail.com>
2024-09-25 11:58:26 +0545

    New file, get_and_put_function added

---
+++ Chapter9/get_and_put_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char c, str[25];
+    cout<< "Enter a character: ";
+    cin.get(c);
+    cout<< endl<< "The entered character is: ";
+    cout.put(c);
+    return 0;
+}
