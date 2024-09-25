1c2d98fab95529649c2f46eb7ab4372bb42194b5
Roshan <roshan9825930688@gmail.com>
2024-09-25 12:05:51 +0545

    New file, get_function_that_takes_whitespace_character added

---
+++ Chapter9/get_function_that_takes_whitespace_character.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+const int MAX = 50;
+char str1[MAX];
+char str2[MAX];
+
+int main()
+{
+    cout<< "Enter a string terminated by newline: "<< endl;
+    cin.get(str1, MAX);  // reads string until new line is reached
+    cout<< "Enter a multiline string terminated by $: "<< endl;
+    cin.get(str2, MAX, '$');  // reads string until '$' or MAX char is reached
+    cout<< "String you entered are: "<< endl;
+    cout<< "String 1: "<< str1<< endl;
+    cout<< "String 2: "<< str2<< endl;
+    return 0;
+}
