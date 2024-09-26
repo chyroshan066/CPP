06508aacb389d9f8f002474021cf9a47420a83e4
Roshan <roshan9825930688@gmail.com>
2024-09-26 12:41:40 +0545

    New file, setf_function added

---
+++ Chapter9/setf_function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout.width(8);
+    cout.fill('#');
+    cout.setf(ios::left, ios::adjustfield);
+    cout<< 456;
+    return 0;
+}
